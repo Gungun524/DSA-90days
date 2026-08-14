6532311383649bc17a406d37caf7422356b63f63
GUNGUN <bhandarigungun3@gmail.com>
2026-08-14 23:33:58 +0530

    Diagonal Sum in 2D Matrix.

---
+++ Diagonalsum.cpp
@@ -0,0 +1,47 @@
+//Problem : Diagonal sum  of 2d  n*n matrix.
+//Approach1: with Timecomplexity = O(n^2).
+#include <iostream>
+using namespace std ;
+void diagonalsum1(int mat[][4],int n){
+    int sum = 0;
+    for(int i = 0;i<n;i++){
+        for(int j = 0;j<n;j++){
+            if(i==j){
+                sum+=mat[i][j];
+            }
+            else if(j==n-i-1){
+                sum+=mat[i][j];
+            }
+
+        }
+    }
+    cout<<"Diagonal Sum of the matrix:"<<sum;
+}
+//Approach2:  with Timecomplexity = O(n).
+void diagnalsum2(int mat[][4],int n){
+    int sum = 0;
+    for(int i = 0;i<n;i++){
+        sum+=mat[i][i];
+        if(i!=n-i-1){
+            sum+=mat[i][n-i-1];
+        }
+    }
+    cout<<"Diagonal Sum of the matrix:"<<sum;
+}
+
+int main() {
+    int matrix[4][4] = {
+        {1,2,3,4},
+        {5,6,7,8},
+        {9,10,11,12},
+        {13,14,15,16}
+    };
+    cout<<"approach1"<<endl;
+    diagonalsum1(matrix, 4);
+    cout<<endl;
+    cout<<"approach2"<<endl;
+    diagonalsum1(matrix, 4);
+    
+
+    return 0;
+}
